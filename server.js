1c786fa8a0854501c7e14bae47b4d25afec65667
jakep5 <jakepagel1@yahoo.com>
2019-11-06 16:35:01 -0600

    format for deployment

--- server.js
+++ server.js
@@ -7,7 +7,9 @@ require('dotenv').config();
 
 const app = express();
 
-app.use(morgan('dev'));
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
+app.use(morgan(morganSetting));
+
 app.use(helmet());
 app.use(cors());
 
@@ -15,7 +17,6 @@ app.use(function validateBearerToken(req, res, next) {
     const apiToken = process.env.API_TOKEN;
     const authToken = req.get('Authorization');
 
-    console.log('validating')
 
     if(!authToken || authToken.split(' ')[1] !== apiToken) {
         return res.status(401).json({error: 'Unauthorized request'})
@@ -52,8 +53,16 @@ function handleGetMovies(req, res) {
     res.json(response);
 }
 
-const PORT = 8000;
-
-app.listen(PORT, () => {
-    console.log(`Server listening at http://localhost:${PORT}`);
+app.use((error, req, res, next) => {
+    let response
+    if (process.env.NODE_ENV === 'production') {
+        response = { error: { message : 'server-error' }}
+    } else {
+        response = {error}
+    }
+    res.status(500).json(response)
 })
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {});
